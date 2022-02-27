9110ba1c7995f428611b6bda826885af8275390a
jonny desktop <jon@uslms.com>
2022-02-27 13:44:25 -0700

    got the home page to pull up

---
+++ src/pages/defaultBoard/defaultBoard.js
@@ -0,0 +1,16 @@
+const defaultBoard = () => {
+    return (
+        <div>
+            <button>Add component</button>
+            <ui>
+                <li>example component</li>
+                <li>example component</li>
+                <li>example component</li>
+                <li>example component</li>
+                <li>example component</li>
+            </ui>
+        </div>
+    )
+}
+
+export default defaultBoard;
