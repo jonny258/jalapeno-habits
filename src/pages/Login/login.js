51e3d5827f4ede778fa97b63f50d2b5163efa862
jonny desktop <jon@uslms.com>
2022-02-27 13:59:16 -0700

    got the links in homepage and added profile.js and login.js

---
+++ src/pages/Login/login.js
@@ -0,0 +1,18 @@
+const Login = () => {
+    return(
+        <div>
+            <section>
+                <div>
+                    <label>Username</label>
+                    <input />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input />
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/pages/profile/profile.js
@@ -0,0 +1,22 @@
+const Profile = () => {
+    return (
+        <div>
+            <section>
+                <div>
+                    <label>Username</label>
+                    <label>filler txt</label>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <label>filler txt</label>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <label>filler txt</label>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Profile;
